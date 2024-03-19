02bbf9764ab66d2d30f70693c2ee5c446fe47116
ktx-vaidehi <vaidehi.akhani@kiara.tech>
2024-03-19 11:36:14 +0530

    same name as backend

--- web/src/views/Dashboards/addPanel/AddPanel.vue
+++ web/src/views/Dashboards/addPanel/AddPanel.vue
@@ -947,7 +947,7 @@ export default defineComponent({
             }
             if (
               dashboardData.data.queries[dashboardData.layout.currentQueryIndex]
-                .fields.valueForMaps == null
+                .fields.value_for_maps == null
             ) {
               errors.push("Add one field for the value");
             }
